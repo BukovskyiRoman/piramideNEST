56845745de47d8d31df6f8439d18e42f97d1a58b
root <peacerock89@gmail.com>
2023-03-10 13:30:53 +0200

    Commit to dev, and merge to main

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -102,7 +102,7 @@ export class AuthController {
         return {
             users: 5,
             greeting: "Hello dear friend!",
-            deploy: 8
+            deploy: 9
         }
     }
 }
